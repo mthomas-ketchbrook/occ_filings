adc9d5ffa1d88313cbe53e06a9d54312d6381bca
mthomas-ketchbrook <52801195+mthomas-ketchbrook@users.noreply.github.com>
2020-04-23 16:23:58 -0400

    Added geocoding

--- selenium_scraper_branch.py
+++ selenium_scraper_branch.py
@@ -6,6 +6,8 @@ import bs4 as bs
 from selenium.webdriver.support.ui import Select
 import sqlite3
 import datetime
+import geopy
+from geopy.extra.rate_limiter import RateLimiter
 
 # Estbalish browsing session
 webdriver = "C:/Users/18602/AppData/Local/Programs/Python/Python37/Lib/site-packages/selenium/webdriver/chrome/chromedriver.exe"
@@ -107,6 +109,26 @@ df1 = df1.drop(df1.columns[[0]], axis = 1)
 
 # Remove whitespace in column names
 df1.columns = df1.columns.str.replace(' ', '')
+
+# Create new "FullAddress" temporary column
+df1['FullAddress'] = df1['Location'] + "," + df1['City'] + "," + df1['State']
+
+# Set up API calls
+locator = geopy.Nominatim(user_agent = "myGeocoder")
+geocode = RateLimiter(locator.geocode, min_delay_seconds = 1)
+
+# Get Lat/Lon/Ele data
+coords = df1['FullAddress'].apply(geocode)
+
+# Create longitude, latitude and altitude from location column (returns tuple)
+coords2 = coords.apply(lambda loc: tuple(loc.point) if loc else None)
+
+# print(coords2)
+
+df1['Coordinates'] = coords2
+
+# Drop the 'FullAddress' temporary column
+df1 = df1.drop(df1.columns[[10]], axis = 1)
 
 # Write DataFrame to a .csv
 # df1.to_csv(r'C:/Users/18602/Desktop/test_selenium.csv', index = False, header = True)
