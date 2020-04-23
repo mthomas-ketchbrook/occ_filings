7f4db37e5041b3b423f7ff6af3e482132f2534c0
mthomas-ketchbrook <52801195+mthomas-ketchbrook@users.noreply.github.com>
2020-04-23 17:21:18 -0400

    Formatting code

--- sqlite_setup.R
+++ sqlite_setup.R
@@ -7,43 +7,45 @@ con <- RSQLite::dbConnect(
   "occ-warehouse.sqlite"
 )
 
-headers_hq <- tibble::tibble(
-  Action = character(0), 
-  Date = character(0), 
-  Type = character(0), 
-  ApplicationNumber = character(0), 
-  BankName = character(0), 
-  Location = character(0), 
-  City = character(0), 
-  State = character(0), 
-  County = character(0), 
-  EndCmtPd  = character(0)
-)
-
-headers_branch <- tibble::tibble(
-  Action = character(0), 
-  Date = character(0), 
-  Type = character(0), 
-  ApplicationNumber = character(0), 
-  BranchName = character(0), 
-  Location = character(0), 
-  City = character(0), 
-  State = character(0), 
-  County = character(0), 
-  EndCmtPd  = character(0)
-)
-
-# Create HQ Table
-# RSQLite::dbWriteTable(
-#   conn = con, 
-#   name = "OCCFilingsHQ", 
-#   value = headers_hq
+# headers_hq <- tibble::tibble(
+#   Action = character(0), 
+#   Date = character(0), 
+#   Type = character(0), 
+#   ApplicationNumber = character(0), 
+#   BankName = character(0), 
+#   Location = character(0), 
+#   City = character(0), 
+#   State = character(0), 
+#   County = character(0), 
+#   EndCmtPd  = character(0), 
+#   Coordinates = character(0)
+# )
+# 
+# headers_branch <- tibble::tibble(
+#   Action = character(0), 
+#   Date = character(0), 
+#   Type = character(0), 
+#   ApplicationNumber = character(0), 
+#   BranchName = character(0), 
+#   Location = character(0), 
+#   City = character(0), 
+#   State = character(0), 
+#   County = character(0), 
+#   EndCmtPd  = character(0), 
+#   Coordinates = character(0)
 # )
 
-# Create Branch Table
+# # Create HQ Table
 # RSQLite::dbWriteTable(
-#   conn = con, 
-#   name = "OCCFilingsBranch", 
+#   conn = con,
+#   name = "OCCFilingsHQ",
+#   value = headers_hq
+# )
+# 
+# # Create Branch Table
+# RSQLite::dbWriteTable(
+#   conn = con,
+#   name = "OCCFilingsBranch",
 #   value = headers_branch
 # )
 
