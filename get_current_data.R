b86f15e2a2438a5f47e4dac73fddde46bc9bc726
mthomas-ketchbrook <52801195+mthomas-ketchbrook@users.noreply.github.com>
2020-05-05 09:18:58 -0400

    Keep only 1 month of data in dashboard

--- get_current_data.R
+++ get_current_data.R
@@ -1,9 +1,11 @@
 # Convert SQLite database to .csv file
 library(tidyverse)
 library(RSQLite)
+library(lubridate)
 
 source("funs.R")
 
-data <- get_occ_data()
+data <- get_occ_data() %>% 
+  dplyr::filter(Date >= max(Date) %m-% months(1))   # Keep only data in last month
 
 readr::write_csv(data, path = "current_data.csv")
