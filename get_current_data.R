87bc779fab7b4f9b035f094c8036ea98983607ef
mthomas-ketchbrook <52801195+mthomas-ketchbrook@users.noreply.github.com>
2020-06-29 22:37:49 -0400

    Put each argument on its own line

--- get_current_data.R
+++ get_current_data.R
@@ -8,4 +8,7 @@ source("funs.R")
 data <- get_occ_data() %>% 
   dplyr::filter(Date >= max(Date) %m-% months(1))   # Keep only data in last month
 
-readr::write_csv(data, path = "current_data.csv")
+readr::write_csv(
+  data, 
+  path = "current_data.csv"
+)
